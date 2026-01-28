5ec09681b75ba0928cdcac8ea6eb4fae28b7fe3a
anfigeno <dolorcriticodevastador@proton.me>
2026-01-28 14:28:47 -0500

    Integracion para rainbow-delimiters.nvim

--- lua/mestizo/init.lua
+++ lua/mestizo/init.lua
@@ -3,7 +3,8 @@ local M = {}
 ---@return table<table<string, string>>
 local function obtener_integraciones()
 	local integraciones = {}
-	local nombres_de_integraciones = { "escencial", "lsp", "neotree", "fyler", "gitsigns", "telescope" }
+	local nombres_de_integraciones =
+		{ "escencial", "lsp", "neotree", "fyler", "gitsigns", "telescope", "rainbow-delimiters" }
 
 	for _, nombre in ipairs(nombres_de_integraciones) do
 		table.insert(integraciones, require("mestizo.integraciones." .. nombre))
---
+++ lua/mestizo/integraciones/rainbow-delimiters.lua
@@ -0,0 +1,9 @@
+return {
+	RainbowDelimiterRed = { ctermfg = 1 },
+	RainbowDelimiterGreen = { ctermfg = 2 },
+	RainbowDelimiterYellow = { ctermfg = 3 },
+	RainbowDelimiterBlue = { ctermfg = 4 },
+	RainbowDelimiterViolet = { ctermfg = 5 },
+	RainbowDelimiterCyan = { ctermfg = 6 },
+	RainbowDelimiterOrange = { ctermfg = 209 },
+}
