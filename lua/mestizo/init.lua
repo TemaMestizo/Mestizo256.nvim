03ae290d46c2b7e23aaecb26c1e8291b33e4ef1f
anfigeno <dolorcriticodevastador@proton.me>
2026-01-28 14:43:31 -0500

    Integración para which-key.nvim

--- lua/mestizo/init.lua
+++ lua/mestizo/init.lua
@@ -3,8 +3,16 @@ local M = {}
 ---@return table<table<string, string>>
 local function obtener_integraciones()
 	local integraciones = {}
-	local nombres_de_integraciones =
-		{ "escencial", "lsp", "neotree", "fyler", "gitsigns", "telescope", "rainbow-delimiters" }
+	local nombres_de_integraciones = {
+		"escencial",
+		"lsp",
+		"neotree",
+		"fyler",
+		"gitsigns",
+		"telescope",
+		"rainbow-delimiters",
+		"which-key",
+	}
 
 	for _, nombre in ipairs(nombres_de_integraciones) do
 		table.insert(integraciones, require("mestizo.integraciones." .. nombre))
---
+++ lua/mestizo/integraciones/which-key.lua
@@ -0,0 +1,8 @@
+return {
+	WhichKeyNormal = { ctermbg = 0 },
+	WhichKeyBorder = { ctermfg = 1, ctermbg = 0 },
+	WhichKeyTitle = { link = "Title" },
+	WhichKeyDesc = { ctermfg = 254 },
+	WhichKeySeparator = { ctermfg = 244 },
+	WhichKey = { ctermfg = 1 },
+}
